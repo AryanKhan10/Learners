2769dd996855e5f71a1d19449592b28085e8538c
AryanKhan10 <cs.aryan.uet@gmail.com>
2025-02-25 19:06:04 +0500

    resolve an issue in section  API

--- vite-project/src/components/Dashboard/AddCourse/NestedView.jsx
+++ vite-project/src/components/Dashboard/AddCourse/NestedView.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Trash2, Edit2, ChevronDown, PanelTopOpen, Plus } from "lucide-react"
