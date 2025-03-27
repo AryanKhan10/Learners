9d08a2a043734b0b701e5ec027e05a19c2505d0d
AryanKhan10 <cs.aryan.uet@gmail.com>
2025-03-27 17:57:25 +0500

    handled multiple enrollments

--- vite-project/src/components/Dashboard/Cart/RenderTotalAmmount.jsx
+++ vite-project/src/components/Dashboard/Cart/RenderTotalAmmount.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'react-hot-toast'
+import { useNavigate } from 'react-router-dom'
+import { setCourse } from '../../../slices/course'
+import { enrollInCourses } from '../../../services/course'
+import { resetCart } from '../../../slices/cart'
 function RenderTotaaAmmount() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+    const {token} = useSelector(state=>state.auth)
     const {total} = useSelector(state=>state.cart)
-    const handleBuyCourse =()=>{
-        
+    const {cart} = useSelector(state=>state.cart)
+    const [loading, setLoading] = useState(false)
+    console.log(cart)
+    const handleBuyCourse = async()=>{
+      setLoading(true)
+      const result= await enrollInCourses(cart,token)
+      if(result){
+        dispatch(setCourse(result))
+            toast.success("Enrolled successfully!")
+            dispatch(resetCart());
+            navigate('/dashboard/enrolled-course')
+
+      }
+      setLoading(false)
     }
   return (
     <div className="mt-8 bg-gray-800 p-4 rounded-lg">
@@ -13,10 +33,11 @@ function RenderTotaaAmmount() {
         <p className="text-2xl font-bold text-white">Rs {total}</p>
       </div>
       <button
+      disabled={loading}
         onClick={handleBuyCourse}
-        className="w-full bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
+        className="w-full cursor-pointer bg-blue-500 text-white py-2 rounded-md hover:bg-blue-600 transition duration-300"
       >
-        Buy Now
+        { !loading ? 'Buy Now': 'Processing'}
       </button>
     </div>
   )
