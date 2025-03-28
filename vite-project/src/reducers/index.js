1a5ff50cfeeededcf2dfcef679d0dc17b1caba29
AryanKhan10 <cs.aryan.uet@gmail.com>
2025-03-28 10:12:00 +0500

    View Course slice added for students

--- vite-project/src/reducers/index.js
+++ vite-project/src/reducers/index.js
@@ -4,12 +4,14 @@ import profile from '../slices/profile.js'
 import cart from '../slices/cart.js'
 import course from '../slices/course.js'
 import enrolled from '../slices/enrolled.js'
+import view from '../slices/viewCourse.js'
 const rootReducer =  combineReducers({
   auth,
   profile,
   cart,
   course,
   enrolled,
+  view
 })
 
 export default rootReducer;
