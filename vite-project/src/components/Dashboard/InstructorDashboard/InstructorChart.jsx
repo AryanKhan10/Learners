f95783e4f02f7d0b51e8113dd589073264f969ca
AryanKhan10 <cs.aryan.uet@gmail.com>
2025-04-20 14:28:52 +0500

    instructor chart created

--- vite-project/src/components/Dashboard/InstructorDashboard/InstructorChart.jsx
+++ vite-project/src/components/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 
-function InstructorChart() {
+ChartJS.register(ArcElement, Tooltip, Legend);
+function InstructorChart({courses}) {
 
     const [currChart, setCurChart] = useState("students")
     // create random colors
     const getRandomColor = (nColors)=>{
         const colors =[];
         for (let i=0; i<nColors; i++){
-            const color= `rgb(${math.floor(Math.random *256)}, ${math.floor(Math.random *256)}, ${math.floor(Math.random *256)})`
+            const color= `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random() *256)}, ${Math.floor(Math.random() *256)})`
+            console.log(color)
             colors.push(color)
         }
         return colors;
@@ -15,10 +19,57 @@ function InstructorChart() {
 
     //create chart data for student
 
+    const chartDataForStudents ={
+        labels: courses.map((course)=> course.courseTitle),
+        datasets: [
+            {
+                data:courses.map((course)=> course.totalStudentsEnrolled), 
+                backgroundColor:getRandomColor(courses.length),
+                borderColor:getRandomColor(courses.length),
+                borderWidth: 1,
+                hoverOffset: 4,
+
+            }
+        ]
+    }
+    
     //create chart data for income
+    const chartDataForIncome = {
+        labels: courses.map((course)=> course.courseTitle),
+        datasets: [
+            {
+                data:courses.map((course)=> course.totalAmount), 
+                backgroundColor:getRandomColor(courses.length),
+                borderColor:getRandomColor(courses.length),
+                borderWidth: 1,
+                hoverOffset: 4,
+            }
+        ]
+    }
+    console.log(chartDataForStudents)
+    console.log(chartDataForIncome)
+    const options ={
+
+    };
+
   return (
     <div>
-      
+      <div>
+        <p>Visualize</p>
+        <div>
+            <button
+                onClick={()=> setCurChart("students")}>
+                Student
+            </button>
+            <button
+                onClick={()=>setCurChart("Income")}>
+                Income
+            </button>
+        </div>
+        <div>
+            <Pie data={currChart === "students" ? chartDataForStudents : chartDataForIncome} options={options} />
+        </div>
+      </div>
     </div>
   )
 }
