5c5e159e5f7a5bd703229ff63f52f3d66a78c4bb
AryanKhan10 <cs.aryan.uet@gmail.com>
2025-05-06 17:21:18 +0500

    frontend url adjusted in api.js

--- vite-project/src/services/apis.js
+++ vite-project/src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BASE_URL
+const BASE_URL = `${import.meta.env.VITE_BASE_URL}/api/v1`;
 // console.log(import.meta.env.VITE_BASE_URL)
 export const categories = {
     CATEGORIES_API: `${BASE_URL}/course/getCategories`
